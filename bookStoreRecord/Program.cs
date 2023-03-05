4cdcea0d97bd5ddbf8ddf2461b09fa861012f3cd
Owner <Owner@DESKTOP-D957Q9E>
2023-03-05 11:42:06 -0500

    Add project files.

---
+++ bookStoreRecord/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace bookStore
+{
+    class Program
+    {
+        public record Book(int ID, string Title, string Author, double Price);
+
+        public static void Main()
+        {
+            Book harry_potter = new(1, "Harry Potter and the Chamber of Secrets", "JK Rowling", 30);
+            Console.WriteLine(harry_potter);
+
+        }
+    }
+}
